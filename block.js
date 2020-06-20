aa607ac10bb65b48acd83e8bc7429e6fea09e044
nandemotoken <nandemotoken@gmail.com>
2020-06-20 12:17:58 +0000

    typo

--- block.js
+++ block.js
@@ -1,6 +1,6 @@
 window.onload = async () =>{
     const portis = new Portis('0e7a7a44-0ca0-48b9-888c-1ec05d30a82f', 'core');
-    const web3 = new Web3(portis.provider);
+    const web3p = new Web3(portis.provider);
     const Address =  "0x73b02AcDd380FA233e1263a0518f50DcA5EBfB5D";
     // console.log(Address);
     const account = await web3p.eth.getAccounts();
